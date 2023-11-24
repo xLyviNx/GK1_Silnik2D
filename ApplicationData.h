7063098f9bc9c2310e72f1056dddc93bc4b0bb6c
LyviN <LyviNSFM@gmail.com>
2023-11-24 09:04:37 +0100

    .

--- ApplicationData.h
+++ ApplicationData.h
@@ -1,27 +1,60 @@
 // ApplicationData.h
 #pragma once
+
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics.hpp>
 #include <string>
 using namespace std;
 namespace Engine2D
 {
+    /**
+     * @brief Enumeracja reprezentuj¹ca ró¿ne tryby wyœwietlania aplikacji.
+     */
     enum DisplayMode
     {
-        Windowed,
-        Fullscreen,
+        Windowed,    ///< Tryb okienkowy.
+        Fullscreen,  ///< Tryb pe³noekranowy.
     };
+
+    /**
+     * @brief Struktura przechowuj¹ca dane aplikacji.
+     */
     struct ApplicationData
     {
-        sf::String ApplicationName;
-        sf::Vector2<int> WindowSize;
-        DisplayMode displayMode;
-        int maxFramerate;
-        struct ApplicationData(sf::String appName, int Width, int Height, DisplayMode dispMode, int maxFps);
-        struct ApplicationData();
-    };
-    extern ApplicationData* appData;
-    void LoadAppData();
-    void createApplicationDataIfNone(ApplicationData* data);
+        sf::String ApplicationName; ///< Nazwa aplikacji.
+        sf::Vector2<int> WindowSize; ///< Rozmiar okna aplikacji.
+        DisplayMode displayMode; ///< Tryb wyœwietlania.
+        int maxFramerate; ///< Maksymalna liczba klatek na sekundê.
 
+        /**
+         * @brief Konstruktor struktury ApplicationData z parametrami.
+         * @param appName Nazwa aplikacji.
+         * @param Width Szerokoœæ okna.
+         * @param Height Wysokoœæ okna.
+         * @param dispMode Tryb wyœwietlania.
+         * @param maxFps Maksymalna liczba klatek na sekundê.
+         */
+        ApplicationData(sf::String appName, int Width, int Height, DisplayMode dispMode, int maxFps);
+
+        /**
+         * @brief Domyœlny konstruktor struktury ApplicationData.
+         */
+        ApplicationData();
+    };
+
+    /**
+     * @brief WskaŸnik na obiekt ApplicationData, przechowuj¹cy globalne dane aplikacji.
+     */
+    extern ApplicationData* appData;
+
+    /**
+     * @brief Funkcja odpowiedzialna za wczytanie danych aplikacji.
+     */
+    void LoadAppData();
+
+    /**
+     * @brief Funkcja tworz¹ca obiekt ApplicationData, jeœli nie istnieje.
+     * @param data WskaŸnik na dane aplikacji.
+     */
+    void createApplicationDataIfNone(ApplicationData* data);
 }
--- Calculations.h
+++ Calculations.h
@@ -4,11 +4,39 @@
 
 namespace Engine2D
 {
-	class Calculations {
-	public:
-		static sf::Vector2f Vector2f_Normalize(sf::Vector2f Vector);
-		static float Vector2f_Magnitude(sf::Vector2f Vector);
-		static float dotProduct(const sf::Vector2f& a, const sf::Vector2f& b);
-		static int Random(int min, int max);
-	};
+    /**
+     * @brief Klasa zawieraj¹ca ró¿ne matematyczne operacje.
+     */
+    class Calculations {
+    public:
+        /**
+         * @brief Normalizuje wektor sf::Vector2f.
+         * @param Vector Wektor do znormalizowania.
+         * @return Znormalizowany wektor sf::Vector2f.
+         */
+        static sf::Vector2f Vector2f_Normalize(sf::Vector2f Vector);
+
+        /**
+         * @brief Oblicza d³ugoœæ wektora sf::Vector2f.
+         * @param Vector Wektor, którego d³ugoœæ ma byæ obliczona.
+         * @return D³ugoœæ wektora.
+         */
+        static float Vector2f_Magnitude(sf::Vector2f Vector);
+
+        /**
+         * @brief Oblicza iloczyn skalarny dwóch wektorów sf::Vector2f.
+         * @param a Pierwszy wektor.
+         * @param b Drugi wektor.
+         * @return Iloczyn skalarny.
+         */
+        static float dotProduct(const sf::Vector2f& a, const sf::Vector2f& b);
+
+        /**
+         * @brief Generuje losow¹ liczbê ca³kowit¹ w okreœlonym zakresie.
+         * @param min Dolna granica zakresu.
+         * @param max Górna granica zakresu.
+         * @return Wygenerowana liczba losowa.
+         */
+        static int Random(int min, int max);
+    };
 }
